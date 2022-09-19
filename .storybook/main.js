f0aaaf9c29f1dfde97e7b688c6e61088e45560c6
Poggioli <joaopogiolli@gmail.com>
2022-09-19 08:12:26 -0300

    add storybook

---
+++ .storybook/main.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions"
+  ],
+  webpackFinal: async config => {
+    config.resolve.alias["@components"] = path.resolve(__dirname, '../src/components')
+    config.resolve.alias["@utils"] = path.resolve(__dirname, '../src/utils')
+    config.resolve.alias["@tokens"] = path.resolve(__dirname, '../src/tokens/index.ts')
+    return config
+ },
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "@storybook/builder-webpack5"
+  }
+}
