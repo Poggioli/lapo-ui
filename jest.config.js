82b4bf4c66d210f0cfbcc97724142127ad87d5d9
Poggioli <joaopogiolli@gmail.com>
2022-10-10 12:11:42 -0300

    add test do Flex component

--- jest.config.js
+++ jest.config.js
@@ -2,12 +2,10 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   moduleNameMapper: {
-    '@components/(.*)': '<rootDir>/src/components/core/$1',
-    '@layout/(.*)': '<rootDir>/src/components/layout/$1',
-    '@utils/(.*)': '<rootDir>/src/utils/$1',
+    '@lapo': '<rootDir>/src/core/lapo.config.ts',
+    '@layout/(.*)': '<rootDir>/src/layout/$1',
     '@tokens': '<rootDir>/src/tokens/index.ts',
-    '@emotion/core': '<rootDir>/src/types/emotion.d.ts',
-    '@lapo': '<rootDir>/src/core/lapo.config.ts'
+    '@emotion/core': '<rootDir>/src/types/emotion.d.ts'
   },
   setupFilesAfterEnv: ['<rootDir>/jest-setup.ts'],
   testEnvironment: 'jest-environment-jsdom',
---
+++ src/layout/Flex/Flex.spec.tsx
@@ -0,0 +1,35 @@
+import { LapoVariants } from '@lapo';
+import { render, RenderResult } from '@testing-library/react';
+import React from 'react';
+import { Flex } from './Flex';
+
+describe('Given a flex', () => {
+  let rendered: RenderResult;
+  let flexComponent: HTMLElement;
+
+  const mount = ({ ...props }: LapoVariants<typeof Flex>) => {
+    rendered = render(<Flex { ...props } />);
+    flexComponent = rendered.container;
+  }
+
+  it('should render a div', () => {
+    mount({});
+    expect(flexComponent).toBeVisible();
+  });
+
+  it('should render a div with custom variants', () => {
+    mount({
+      flexDirection: 'column',
+      flexWrap: 'wrap',
+      justifyContent: 'center',
+      alignItems: 'center',
+      alignContent: 'flex-end',
+    });
+    expect(flexComponent).toBeTruthy();
+  });
+
+  it('should render a div with default properties', () => {
+    mount({});
+    expect(flexComponent).toBeTruthy();
+  });
+});
