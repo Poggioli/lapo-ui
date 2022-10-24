62ed250920816015836f7f288efbc10a97260da7
Poggioli <joaopogiolli@gmail.com>
2022-10-24 09:14:09 -0300

    Add dropdown component

---
+++ src/components/Dropdown/Dropdown.spec.tsx
@@ -0,0 +1,83 @@
+import { render, RenderResult } from '@testing-library/react';
+import { axe } from 'jest-axe';
+import React from 'react';
+import {
+  Dropdown,
+  DropdownContent,
+  DropdownGroup,
+  DropdownGroupLabel,
+  DropdownItem,
+  DropdownSelectedIndicator,
+  DropdownSeparator,
+  DropdownText,
+  DropdownTrigger,
+  DropdownValue
+} from './Dropdown';
+
+describe('Given a <Dropdown /> component', () => {
+  let rendered: RenderResult;
+  type DropdownIds = {
+    icon?: string;
+    trigger?: string;
+    groupLabel?: string;
+    selectIndicator?: string;
+    selectItem?: string;
+    selectItemText?: string;
+    content?: string;
+    root?: string;
+  };
+
+  const mount = ({
+    trigger,
+    groupLabel,
+    selectIndicator,
+    selectItem,
+    selectItemText,
+    content,
+    root
+  }: DropdownIds) => {
+    rendered = render(
+      <Dropdown id={root}>
+        <DropdownTrigger id={trigger}>
+          <DropdownValue placeholder="Selecect an animal" />
+        </DropdownTrigger>
+        <DropdownContent id={content}>
+          <DropdownGroup>
+            <DropdownGroupLabel id={groupLabel}>Birds</DropdownGroupLabel>
+            <DropdownItem id={`${selectItem}-1`} value="cockatiel" disabled={true}>
+              <DropdownSelectedIndicator id={`${selectIndicator}-1`} />
+              <DropdownText id={`${selectItemText}-1`}>Cockatiel</DropdownText>
+            </DropdownItem>
+            <DropdownItem id={`${selectItem}-2`} value="budgerigar">
+              <DropdownSelectedIndicator id={`${selectIndicator}-2`} />
+              <DropdownText id={`${selectItemText}-2`}>Budgerigar</DropdownText>
+            </DropdownItem>
+            <DropdownItem id={`${selectItem}-3`} value="cockatoo">
+              <DropdownSelectedIndicator id={`${selectIndicator}-3`} />
+              <DropdownText id={`${selectItemText}-3`}>Cockatoo</DropdownText>
+            </DropdownItem>
+            <DropdownItem id={`${selectItem}-3`} value="gray-parrot">
+              <DropdownSelectedIndicator id={`${selectIndicator}-3`} />
+              <DropdownText id={`${selectItemText}-3`}>Gray parrot</DropdownText>
+            </DropdownItem>
+            <DropdownItem id={`${selectItem}-4`} value="blue-parrot">
+              <DropdownSelectedIndicator id={`${selectIndicator}-4`} />
+              <DropdownText id={`${selectItemText}-4`}>Blue parrot</DropdownText>
+            </DropdownItem>
+          </DropdownGroup>
+          <DropdownSeparator />
+        </DropdownContent>
+      </Dropdown>
+    );
+  };
+
+  it('should render', async () => {
+    mount({});
+    expect(rendered).toBeTruthy();
+  });
+
+  xit('should have no a11y violations', async () => {
+    mount({});
+    expect(await axe(rendered.container)).toHaveNoViolations();
+  });
+});
---
+++ src/components/Dropdown/Dropdown.stories.tsx
@@ -0,0 +1,96 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import React from 'react';
+import {
+  Dropdown,
+  DropdownContent, DropdownGroup, DropdownGroupLabel, DropdownItem, DropdownSelectedIndicator, DropdownSeparator, DropdownText, DropdownTrigger,
+  DropdownValue
+} from './Dropdown';
+
+export default {
+  title: 'Componentes/components/Dropdown',
+  component: Dropdown,
+  argTypes: {
+    shape: {
+      options: ['solid', 'ghost', 'standard'],
+      control: { type: 'select' },
+      defaultValue: 'solid'
+    },
+    variant: {
+      options: ['primary', 'danger'],
+      control: { type: 'select' },
+      defaultValue: 'primary'
+    },
+    disabled: {
+      type: 'boolean',
+      defaultValue: false
+    },
+    ref: {
+      table: {
+        disable: true
+      }
+    },
+    css: {
+      table: {
+        disable: true
+      }
+    },
+    as: {
+      table: {
+        disable: true
+      }
+    }
+  }
+} as ComponentMeta<typeof Dropdown>;
+
+const Template: ComponentStory<typeof DropdownTrigger> = ({ ...args }) => {
+  return (
+    <Dropdown>
+      <DropdownTrigger {...args}>
+        <DropdownValue placeholder="Selecione um animal" />
+      </DropdownTrigger>
+      <DropdownContent>
+        <DropdownGroup>
+          <DropdownGroupLabel>Aves</DropdownGroupLabel>
+          <DropdownItem value="calopsita" disabled={true}>
+            <DropdownSelectedIndicator />
+            <DropdownText>Calopsita</DropdownText>
+          </DropdownItem>
+          <DropdownItem value="periquito-australiano">
+            <DropdownSelectedIndicator />
+            <DropdownText>Periquito Australiano</DropdownText>
+          </DropdownItem>
+          <DropdownItem value="cacatua">
+            <DropdownSelectedIndicator />
+            <DropdownText>Cacatua</DropdownText>
+          </DropdownItem>
+          <DropdownItem value="papagaio-do-congo">
+            <DropdownSelectedIndicator />
+            <DropdownText>Papagaio do Congo</DropdownText>
+          </DropdownItem>
+          <DropdownItem value="papagaio-verdadeiro">
+            <DropdownSelectedIndicator />
+            <DropdownText>Papagaio Verdadeiro</DropdownText>
+          </DropdownItem>
+        </DropdownGroup>
+        <DropdownSeparator />
+        <DropdownGroup>
+          <DropdownGroupLabel>Cachorros</DropdownGroupLabel>
+          <DropdownItem value="poodle">
+            <DropdownSelectedIndicator />
+            <DropdownText>Poodle</DropdownText>
+          </DropdownItem>
+          <DropdownItem value="pastor-alemao">
+            <DropdownSelectedIndicator />
+            <DropdownText>Pastor Alemão</DropdownText>
+          </DropdownItem>
+          <DropdownItem value="pitbull">
+            <DropdownSelectedIndicator />
+            <DropdownText>Pitbull</DropdownText>
+          </DropdownItem>
+        </DropdownGroup>
+      </DropdownContent>
+    </Dropdown>
+  );
+};
+
+export const Default = Template.bind({});
